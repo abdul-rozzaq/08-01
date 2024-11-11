1cdf9bb4b0035be35a4dfa66d0acc803115413a6
Abdurazzoq Abdusalomov <rayyan.dev@mail.ru>
2024-11-11 19:38:00 +0500

    asda

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Article, Author, Category, Comment
+
+admin.site.register(Category)
+admin.site.register(Author)
+admin.site.register(Article)
+admin.site.register(Comment)
