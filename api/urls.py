4f572db3c7fba5bbe3f87881273882ae7d8577b7
Abdurazzoq Abdusalomov <rayyan.dev@mail.ru>
2024-11-15 09:23:08 +0500

    asdasdxzczzx

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from .views import ArticleAPIView, AuthorAPIView, CategoryAPIView, CommentAPIView, home_page
+from .views import ArticleListCreateAPIView, ArticleRetrieveUpdateDestroyAPIView, AuthorAPIView, CategoryListCreateAPIView, CategoryRetrieveUpdateDestroyAPIView, CommentAPIView, HomePageView
 
 urlpatterns = [
-    path("", home_page),
-    path("categories/", CategoryAPIView.as_view()),
-    path("authors/", AuthorAPIView.as_view()),
-    path("articles/", ArticleAPIView.as_view()),
-    path("comments/", CommentAPIView.as_view()),
+    path("", HomePageView.as_view(), name="home-page"),
+    path("categories/", CategoryListCreateAPIView.as_view(), name="categories-list"),
+    path("categories/<int:pk>/", CategoryRetrieveUpdateDestroyAPIView.as_view(), name="categories-retrieve"),
+    path("articles/", ArticleListCreateAPIView.as_view(), name="articles-list"),
+    path("articles/<int:pk>/", ArticleRetrieveUpdateDestroyAPIView.as_view(), name="articles-retrieve"),
+    path("authors/", AuthorAPIView.as_view(), name="authors-list"),
+    path("comments/", CommentAPIView.as_view(), name="comments-list"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,38 +1,49 @@
-from rest_framework.decorators import api_view
-from rest_framework.generics import ListAPIView
+from rest_framework import generics
 from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework.views import APIView
 
 from .models import Article, Author, Category, Comment
 from .serializers import ArticleSerializer, AuthorSerializer, CategorySerializer, CommentSerializer
 
 
-class CategoryAPIView(ListAPIView):
+class CategoryListCreateAPIView(generics.ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
 
-class AuthorAPIView(ListAPIView):
-    queryset = Author.objects.all()
-    serializer_class = AuthorSerializer
+class CategoryRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
 
 
-class ArticleAPIView(ListAPIView):
+class ArticleListCreateAPIView(generics.ListCreateAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
 
 
-class CommentAPIView(ListAPIView):
+class ArticleRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+
+
+class AuthorAPIView(generics.ListAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+
+class CommentAPIView(generics.ListAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
 
 
-@api_view(["GET"])
-def home_page(request):
-    return Response(
-        {
-            "categories": "/api/categories/",
-            "authors": "/api/authors/",
-            "articles": "/api/articles/",
-            "comments": "/api/comments/",
-        }
-    )
+class HomePageView(APIView):
+    def get(self, request, format=None):
+        return Response(
+            {
+                "categories": reverse("categories-list", request=request, format=format),
+                "authors": reverse("authors-list", request=request, format=format),
+                "articles": reverse("articles-list", request=request, format=format),
+                "comments": reverse("comments-list", request=request, format=format),
+            }
+        )
